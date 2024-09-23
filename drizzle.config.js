7ce17345d88f4b794b6860c5a24a88bbcfcfaf8d
Harsh verma <hv72002@gmail.com>
2024-09-23 17:42:29 +0530

    sideBar is done

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ export default {
     schema: "./utils/schema.jsx",
     out: "./drizzle",
     dbCredentials: {
-      url: process.env.NEXT_PUBLIC_DATABASE_URL,
+      url: 'postgresql://Costimize_owner:HShR0O3EzVmQ@ep-solitary-frog-a56k8lci.us-east-2.aws.neon.tech/Costimize?sslmode=require',
       connectionString:
       process.env.NEXT_PUBLIC_DATABASE_URL,
     },
