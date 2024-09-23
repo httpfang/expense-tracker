11c77d169d179c554a07e3bd5d44e6f7b4b3b10f
Harsh verma <hv72002@gmail.com>
2024-09-23 19:47:36 +0530

    upgrade

--- utils/dbConfig.jsx
+++ utils/dbConfig.jsx
@@ -2,6 +2,6 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 const sql = neon(
-  process.env.NEXT_PUBLIC_DATABASE_URL
+  "postgresql://Costimize_owner:HShR0O3EzVmQ@ep-solitary-frog-a56k8lci.us-east-2.aws.neon.tech/Costimize?sslmode=require"
 );
 export const db = drizzle(sql, { schema });
--- utils/index.js
+++ utils/index.js
@@ -5,16 +5,16 @@
  */
 
 const formatNumber = (num) => {
-    if (num >= 1e9) {
-      return (num / 1e9).toFixed(1).replace(/\.0$/, "") + "B";
-    }
-    if (num >= 1e6) {
-      return (num / 1e6).toFixed(1).replace(/\.0$/, "") + "M";
-    }
-    if (num >= 1e3) {
-      return (num / 1e3).toFixed(1).replace(/\.0$/, "") + "K";
-    }
-    return num.toString();
-  };
-  
-  export default formatNumber;
+  if (num >= 1e9) {
+    return (num / 1e9).toFixed(1).replace(/\.0$/, "") + "B";
+  }
+  if (num >= 1e6) {
+    return (num / 1e6).toFixed(1).replace(/\.0$/, "") + "M";
+  }
+  if (num >= 1e3) {
+    return (num / 1e3).toFixed(1).replace(/\.0$/, "") + "K";
+  }
+  return num.toString();
+};
+
+export default formatNumber;
