ad85b600fe6d7b5f0d71f1633bd5b50355bb6a18
Harsh verma <hv72002@gmail.com>
2024-09-24 11:40:10 +0530

    about to complete

--- src/app/(routes)/dashboard/expenses/page.jsx
+++ src/app/(routes)/dashboard/expenses/page.jsx
@@ -1,68 +1,44 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { db } from "@/utils/dbConfig";
-import { Budgets, Expenses } from "@/utils/schema";
-import { Loader } from "lucide-react";
-import moment from "moment";
-import React, { useState } from "react";
-import { toast } from "sonner";
+"use client"
+import { db } from '../../../../../utils/dbConfig';
+import { Budgets, Expenses } from '../../../../../utils/schema';
+import { desc, eq } from 'drizzle-orm';
+import React, { useEffect, useState } from 'react'
+import ExpenseListTable from './_components/ExpenseListTable';
+import { useUser } from '@clerk/nextjs';
 
-function AddExpense({ budgetId, user, refreshData }) {
-  const [name, setName] = useState();
-  const [amount, setAmount] = useState();
-  const [loading, setLoading] = useState(false);
-  /**
-   * Used to Add New Expense
+function ExpensesScreen() {
+
+  const [expensesList,setExpensesList]=useState([]);
+    const {user}=useUser();
+
+    useEffect(()=>{
+        user&&getAllExpenses();
+      },[user])
+    /**
+   * Used to get All expenses belong to users
    */
-  const addNewExpense = async () => {
-    setLoading(true);
-    const result = await db
-      .insert(Expenses)
-      .values({
-        name: name,
-        amount: amount,
-        budgetId: budgetId,
-        createdAt: moment().format("DD/MM/yyy"),
-      })
-      .returning({ insertedId: Budgets.id });
-
-    setAmount("");
-    setName("");
-    if (result) {
-      setLoading(false);
-      refreshData();
-      toast("New Expense Added!");
-    }
-    setLoading(false);
-  };
+  const getAllExpenses=async()=>{
+    const result=await db.select({
+      id:Expenses.id,
+      name:Expenses.name,
+      amount:Expenses.amount,
+      createdAt:Expenses.createdAt
+    }).from(Budgets)
+    .rightJoin(Expenses,eq(Budgets.id,Expenses.budgetId))
+    .where(eq(Budgets.createdBy,user?.primaryEmailAddress.emailAddress))
+    .orderBy(desc(Expenses.id));
+    setExpensesList(result);
+   
+  }
   return (
-    <div className="border p-5 rounded-2xl">
-      <h2 className="font-bold text-lg">Add Expense</h2>
-      <div className="mt-2">
-        <h2 className="text-black font-medium my-1">Expense Name</h2>
-        <Input
-          placeholder="e.g. Bedroom Decor"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+    <div className='p-10'>
+      <h2 className='font-bold text-3xl'>My Expenses</h2>
+
+        <ExpenseListTable refreshData={()=>getAllExpenses()}
+        expensesList={expensesList}
         />
-      </div>
-      <div className="mt-2">
-        <h2 className="text-black font-medium my-1">Expense Amount</h2>
-        <Input
-          placeholder="e.g. 1000"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-      </div>
-      <Button
-        disabled={!(name && amount) || loading}
-        onClick={() => addNewExpense()}
-        className="mt-3 w-full rounded-full"
-      >
-        {loading ? <Loader className="animate-spin" /> : "Add New Expense"}
-      </Button>
     </div>
-  );
+  )
 }
 
-export default AddExpense;
+export default ExpensesScreen
